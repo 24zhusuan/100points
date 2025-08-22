c53d3ad93b6825f8afc15b96c9d6c1f930d379a3
24zhusuan <2697090242@qq.com>
2025-08-22 12:26:29 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -5,8 +5,7 @@ import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter } from 'react-router-dom';
 
 // 从 Clerk 官网获取你的 Publishable Key
-const PUBLISHABLE_KEY = 'VITE_CLERK_PUBLISHABLE_KEY=pk_test_ZW5hYmxlZC1nZWxkaW5nLTc3LmNsZXJrLmFjY291bnRzLmRldiQ'; // 替换成你自己的 Key
-
+const PUBLISHABLE_KEY = 'pk_test_ZW5hYmxlZC1nZWxkaW5nLTc3LmNsZXJrLmFjY291bnRzLmRldiQ';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
@@ -15,4 +14,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </ClerkProvider>
   </React.StrictMode>
+
 );
